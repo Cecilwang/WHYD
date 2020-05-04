f7fd08939f4a66d6fe32f13019caa2d1d0f530f1
cecilwang <cecilwang@126.com>
2020-05-04 14:42:12 +0900

    Add buttons to card.

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,37 @@
+var PLAY_ICON = 'https://img.icons8.com/metro/26/000000/play.png';
+var PAUSE_ICON = 'https://img.icons8.com/metro/26/000000/pause.png';
+var STOP_ICON = 'https://img.icons8.com/metro/26/000000/stop.png';
+
 console.log('Let\'s start.');
 
 window.TrelloPowerUp.initialize({
-  'card-badges': function(t, opts) {
+  'card-badges': t => {
     return t.card('all').then(function(card) {
       console.log(card);
       return [{text: card.idShort}];
     });
+  },
+  'card-detail-badges': () => {},
+  'card-buttons': t => {
+    return [
+      {
+        icon: PLAY_ICON,
+        text: 'Do it!',
+        callback: () => {},
+        condition: 'always'
+      },
+      {
+        icon: PAUSE_ICON,
+        text: 'Pause',
+        callback: () => {},
+        condition: 'always'
+      },
+      {
+        icon: STOP_ICON,
+        text: 'Finished',
+        callback: () => {},
+        condition: 'always'
+      }
+    ]
   }
 });
